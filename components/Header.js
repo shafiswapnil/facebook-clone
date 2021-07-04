330f08461986b39330e975c1b113a47f9d213bc1
Shafi Swapnil <shafiahmedswapnil6494@gmail.com>
2021-07-05 01:35:04 +0600

    added Header component and whitelsited few domains in next.cconfig file

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+
+        <div>
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+
+      {/* Center */}
+
+
+      {/* Right */}
+
+    </div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -9,7 +10,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Facebook</h1>
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
     </div>
   )
 }
