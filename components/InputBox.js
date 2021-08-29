7418384dbfcb2c4e3dff2a9b9a5189f12763b427
Shafi Swapnil <shafiahmedswapnil6494@gmail.com>
2021-08-29 23:37:55 +0600

    working and fixing problem in firebase, firestore

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,13 +1,31 @@
 import { useSession } from "next-auth/client";
 import Image from 'next/image';
+import firebase from 'firebase/app';
+import { Timestamp } from "firebase/firestore";
+import { db } from '../firebase';
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { useRef } from "react";
 
 function InputBox() {
   const [session] = useSession();
+  const inputRef = useRef(null);
 
   const sendPost = e => {
     e.preventDefault();
+
+    if (!inputRef.current.value) return;
+
+    db.collection('posts').doc({
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      // timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: Timestamp.fromDate(new Date("December 10, 1815"))
+    });
+
+    inputRef.current.value = '';
   };
 
   return (
@@ -23,25 +41,25 @@ function InputBox() {
         <form className="flex flex-1">
           <input
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
-            type="text"
+            type="text" ref={inputRef}
             placeholder={`What's on your mind, ${session.user.name}?`}
           />
-          <button type="submit" hidden onClick={sendPost}>Submit</button>
+          <button type="submit" onClick={sendPost}>Submit</button>
         </form>
       </div>
 
-      <div>
-        <div>
+      <div className="flex justify-evenly p-3 border-t">
+        <div className="inputIcon">
           <VideoCameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
 
-        <div>
+        <div className="inputIcon">
           <CameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
         </div>
 
-        <div>
+        <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300" />
           <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
         </div>
